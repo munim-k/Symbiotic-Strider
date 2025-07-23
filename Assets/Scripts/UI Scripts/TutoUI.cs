de84807d79e038374e9cc23902a7c4e03a40741b
Arham Ali <125752248+MuahmmadArhamAli@users.noreply.github.com>
2025-07-23 20:46:46 +0500

    Tutorial Done.

---
+++ Assets/Scripts/UI Scripts/TutoUI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class TutoUI : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> screens;
+    private int index;
+
+    private void Start()
+    {
+        index = 0;
+        Time.timeScale = 1f;
+
+        DisableAllScreens();
+        screens[index].SetActive(true);
+    }
+
+    private void DisableAllScreens()
+    {
+        foreach (GameObject screen in screens)
+        {
+            screen.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            index++;
+            if (index >= screens.Count)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+            else
+            {
+                screens[index].SetActive(true);
+            }
+        }
+    }
+}
